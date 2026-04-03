b873ae7d202e4295b1941c12e9427c744560d627
Y0rFa1se <neargul123@gmail.com>
2026-04-03 21:25:05 +0900

    d

--- SAC/agent.py
+++ SAC/agent.py
@@ -76,6 +76,9 @@ class Agent:
         self.q_optimizer.load_state_dict(checkpoint["q_optimizer"])
         self.policy_optimizer.load_state_dict(checkpoint["policy_optimizer"])
         self.alpha_optimizer.load_state_dict(checkpoint["alpha_optimizer"])
+        
+    def play(self, state):
+        pass
 
     def update(self, batch):
         pass
